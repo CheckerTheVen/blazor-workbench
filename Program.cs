7ff87875dfc9d92c8508bc4c5dce3f67aa8b304b
Can Cekic <ac.cekic@outlook.com>
2024-09-18 10:07:41 +0200

    Added: Configurated server-side logging for Blazor components.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace Workbench
         {
             var builder = WebApplication.CreateBuilder();
 
+            // Additional configurations for the Blazor circuit 
+            // and hub.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents(config =>
                 {
@@ -19,6 +21,12 @@ namespace Workbench
                     config.ClientTimeoutInterval = TimeSpan.FromSeconds(10);
                     config.KeepAliveInterval = TimeSpan.FromSeconds(4);
                 });
+
+            // Configuration for logging.
+            // Add a logger to component by injecting the ILogger<?> interface
+            // with the component class as strongly-typed argument.
+            builder.Logging.AddConsole()
+                .SetMinimumLevel(LogLevel.Trace);
 
             var app = builder.Build();
 
